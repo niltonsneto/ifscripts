48873141c59adedf884b61ffc33c26c6ed509dc9
Nilton dos Santos Neto <niltoncons@gmail.com>
2024-09-10 16:07:56 -0300

    Versão grafica envia_script.sh
    Versão gráfica usando o comando dialog.

--- envia_script.sh
+++ envia_script.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
+# Define o título da janela e o título de fundo
+WINDOW_TITLE="Envia Arquivos"
+BACKTITLE="IFC - Campus Araquari"
 
+    dialog --backtitle "$BACKTITLE" --title "$WINDOW_TITLE" --msgbox "Bem-vindo ao sistema de envio de arquivos via SCP.\n\nEste script irá ajudá-lo a enviar um arquivo para uma lista de IPs." 15 50
 
+FILE_NAME=$(dialog --backtitle "$BACKTITLE" --title "$WINDOW_TITLE" --inputbox "Digite o nome do arquivo:" 10 50 --stdout)
 
-clear
-echo "*********************************************************************"
-echo "*                                                                   *" 
-echo "*                                                                   *" 
-echo "*             SCRIPT PARA TRANSFERENCIA DE ARQUIVOS                 *" 
-echo "*                                                                   *"
-echo "*                                                                   *"
-echo "*********************************************************************"
-echo ""
-echo ""
-echo "Digite o nome do arquivo que deseja transferir:"
-read nome_arquivo
+if [ $? -ne 0 ]; then
+   dialog --backtitle "$BACKTITLE" --title "Cancelada" --sleep 3 --infobox "Operação cancelada!" 5 25
+   clear
+   exit 1  
+fi    
+
 # Verifica se o arquivo existe
-if [ -e "$nome_arquivo" ]; then
-   envia_arquivo "$nome_arquivo"
-else
+if [ ! -e "$FILE_NAME" ]; then
+    dialog --backtitle "$BACKTITLE" --title "Erro!!" --sleep 3 --infobox "O arquivo '$FILE_NAME' não existe." 10 50
     clear
-    echo "ERRO!! O arquivo '$nome_arquivo' não foi encontrado."
+    exit 1
 fi
-
-envia_arquivo(){
 # Arquivo que contém a lista de IPs
  ARQUIVO_IPS="ips.txt"
- NOME_ARQUIVO="$1"
+ clear
 # Arquivo a ser enviado
- ARQUIVO="/home/suporte/$NOME_ARQUIVO"
+ ARQUIVO="/home/suporte/$FILE_NAME"
 # Diretório de destino
  DESTINO="/home/suporte/"
 # Usuário SSH
@@ -37,4 +33,3 @@ while IFS= read -r IP; do
   echo "Enviando $ARQUIVO para $USUARIO@$IP:$DESTINO"
   scp "$ARQUIVO" "$USUARIO@$IP:$DESTINO"
 done < "$ARQUIVO_IPS"
-}
