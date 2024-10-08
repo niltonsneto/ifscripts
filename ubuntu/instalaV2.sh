a28d68a2d103e19035e9307298a2170864ba4e12
Nilton dos Santos Neto <niltoncons@gmail.com>
2024-10-08 13:36:03 -0300

    Update instalaV2.sh

--- ubuntu/instalaV2.sh
+++ ubuntu/instalaV2.sh
@@ -60,13 +60,19 @@ instala_pgAdmin4() {
 
 instala_docker() {
     echo "Instalando Docker..."
-    sudo apt update
-    sudo apt install -y docker.io
-    sudo systemctl start docker
-    sudo systemctl enable docker
-    echo "Instalando Docker Compose..."
-    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
+    # Add Docker's official GPG key:
+    apt update
+    apt install ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  apt update
+  apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
 
 instala_packettracer() {
